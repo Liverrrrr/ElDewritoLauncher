1cfb1772febc703fa03fd39be4371c9c9947f98a
Liver_K <73491850+Liverrrrr@users.noreply.github.com>
2026-01-10 20:38:14 -0600

    update discord invite to new server

--- ElDewritoLauncher.Core/Constants.cs
+++ ElDewritoLauncher.Core/Constants.cs
@@ -8,7 +8,7 @@ namespace EDLauncher.Core
         public const string ApplicationName = "ElDewrito";
         public const string PackageName = "eldewrito";
         public const string PublicKey = "";
-        public const string DiscordUrl = "https://discord.gg/0TKY0SDEUHAWL4sG";
+        public const string DiscordUrl = "https://discord.gg/WqZRTUPmar";
         public const string RedditUrl = "https://www.reddit.com/r/HaloOnline";
 
         public static string GetAppDirectory()
